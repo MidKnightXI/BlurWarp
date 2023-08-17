b5913b53db6c2745d57ccc51e7bf233503042a98
MidKnightXI <romain.bouguet@epitech.eu>
2023-08-17 12:10:38 +0200

    feat: adding proper runner

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,93 +1,89 @@
-use tch::{nn, nn::Module, nn::VarStore, Device, Tensor};
-use image::DynamicImage;
+use std::fs;
+use std::path::Path;
+use serde::{Serialize, Deserialize};
+use tch::{nn, nn::Module, Device, vision};
 
 #[derive(Debug)]
-struct BlurClassifier {
+struct BlurDetectionModel {
     conv1: nn::Conv2D,
     conv2: nn::Conv2D,
-    linear1: nn::Linear,
-    linear2: nn::Linear,
+    fc1: nn::Linear,
+    fc2: nn::Linear,
 }
 
-impl BlurClassifier {
-    fn new(vs: &nn::Path) -> BlurClassifier {
-        let conv1 = nn::conv2d(vs, 1, 32, 5, Default::default());
-        let conv2 = nn::conv2d(vs, 32, 64, 5, Default::default());
-        let linear1 = nn::linear(vs, 1024, 128, Default::default());
-        let linear2 = nn::linear(vs, 128, 2, Default::default());
+impl BlurDetectionModel {
+    fn new(vs: &nn::Path) -> BlurDetectionModel {
+        let conv1 = nn::conv2d(vs, 3, 64, 3, Default::default());
+        let conv2 = nn::conv2d(vs, 64, 128, 3, Default::default());
+        let fc1 = nn::linear(vs / "fc1", 128 * 64 * 64, 256, Default::default());
+        let fc2 = nn::linear(vs / "fc2", 256, 1, Default::default());
 
-        BlurClassifier {
+        BlurDetectionModel {
             conv1,
             conv2,
-            linear1,
-            linear2,
+            fc1,
+            fc2,
         }
     }
 }
 
-impl nn::Module for BlurClassifier {
-    fn forward(&self, xs: &Tensor) -> Tensor {
-        xs.view([-1, 1, 256, 256]) // Adjust dimensions based on your input size
-            .apply(&self.conv1)
-            .max_pool2d_default(2)
-            .relu()
-            .apply(&self.conv2)
-            .max_pool2d_default(2)
-            .relu()
-            .view([-1, 1024])
-            .apply(&self.linear1)
-            .relu()
-            .apply(&self.linear2)
+impl nn::Module for BlurDetectionModel {
+    fn forward(&self, xs: &tch::Tensor) -> tch::Tensor {
+        let xs = xs.apply(&self.conv1).relu().max_pool2d_default(2);
+        let xs = xs.apply(&self.conv2).relu().max_pool2d_default(2);
+        let xs = xs.view((-1, 128 * 64 * 64));
+        let xs = xs.apply(&self.fc1).relu();
+        xs.apply(&self.fc2).sigmoid()
     }
 }
 
+#[derive(Serialize, Deserialize)]
+struct Prediction {
+    status: String,
+    filename: String,
+    detected: Detected,
+}
 
-fn preprocess_image(image: &DynamicImage) -> Tensor {
-    let resized_image = image.resize(256, 256, image::imageops::Nearest).to_luma8();
-
-    let tensor_data: Vec<f32> = resized_image
-        .pixels()
-        .map(|p| p[0] as f32 / 255.0)
-        .collect();
-
-Tensor::of_slice(&tensor_data)
+#[derive(Serialize, Deserialize)]
+struct Detected {
+    blurry: f64,
 }
 
 fn main() {
-    let image_path = "path/to/your/image.jpg"; // Replace with your image path
+    let device = Device::cuda_if_available();
+    let mut vs = tch::nn::VarStore::new(device);
+    let model = BlurDetectionModel::new(&vs.root());
+    vs.load("../blur_detection_model.tch").unwrap();
 
-    let mut vs = VarStore::new(Device::Cpu);
-    let net = BlurClassifier::new(&vs.root());
+    // Make predictions on new data
+    let image_dir = Path::new("./images");
+    let mut predictions = Vec::<Prediction>::new();
 
-    // Load a pre-trained model (adjust the path accordingly)
-    vs.load("path/to/your/pretrained_model.tch")
-        .expect("Failed to load pre-trained model.");
-
-    match image::open(image_path) {
-        Ok(image) => {
-            // Preprocess the image and convert to tensor
-            let image_tensor = preprocess_image(&image);
-
-            // Run the image through the model
-            let output = net.forward(&image_tensor);
-
-            // Get the class probabilities (0 = not blurry, 1 = blurry)
-            let probabilities = output.softmax(-1, tch::Kind::Float);
-
-            let blurry_prob = probabilities.double_value(&[0]);
-            let not_blurry_prob = probabilities.double_value(&[1]);
-
-            println!("Blurry Probability: {}", blurry_prob);
-            println!("Not Blurry Probability: {}", not_blurry_prob);
-
-            if blurry_prob > not_blurry_prob {
-                println!("The image is blurry.");
-            } else {
-                println!("The image is not blurry.");
+    for entry in fs::read_dir(image_dir).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() {
+            match vision::image::load(&path) {
+                Ok(img) => {
+                    let img = img.to_device(device);
+                    let output = model.forward(&img.unsqueeze(0)).double_value(&[]);
+                    predictions.push(Prediction {
+                        status: "success".to_string(),
+                        filename: path.to_str().unwrap().to_string(),
+                        detected: Detected { blurry: output },
+                    });
+                }
+                Err(_) => {
+                    predictions.push(Prediction {
+                        status: "failure".to_string(),
+                        filename: path.to_str().unwrap().to_string(),
+                        detected: Detected { blurry: 0.0 },
+                    });
+                }
             }
         }
-        Err(e) => {
-            eprintln!("Error: {:?}", e);
-        }
     }
+
+    // Output the predictions as a JSON
+    println!("{}", serde_json::to_string_pretty(&predictions).unwrap());
 }
