783e55de037fb0507cc5b5b73fee1e21d5ef2bb0
MidKnightXI <romain.bouguet@epitech.eu>
2023-08-19 16:49:23 +0200

    feat: switching to resnet

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import torch
 import torch.backends.mps
-from torchvision import transforms
-from torchvision import datasets
 from torch.utils.data import DataLoader
 from torch.nn import functional as F
 from torch import nn
+from torchvision import transforms
+from torchvision import datasets
+import torchvision.models as models
 import json
 
 DEVICE = torch.device("cpu")
@@ -16,47 +17,36 @@ elif torch.backends.mps.is_available():
 
 print(f"Running on {DEVICE}")
 
-class BlurDetectionModel(nn.Module):
+class BlurDetectionResNet(nn.Module):
     def __init__(self):
-        super(BlurDetectionModel, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(128 * 64 * 64, 256)  # Adjust based on your input size
-        self.fc2 = nn.Linear(256, 1)
+        super(BlurDetectionResNet, self).__init__()
+        self.resnet = models.resnet18()
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, 1)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)  # Flatten each image in the batch
-        x = F.relu(self.fc1(x))
-        x = torch.sigmoid(self.fc2(x))
-        return x
+        x = self.resnet(x)
+        return torch.sigmoid(x)
 
-# Load the trained model
-model = BlurDetectionModel()
+model = BlurDetectionResNet()
 model.load_state_dict(torch.load('blur_detection_model.tch'))
 model.to(DEVICE)
 
-# Set up data transformations for inference
 transform = transforms.Compose([
     transforms.Resize((256, 256), antialias=True),
     transforms.ToTensor(),
 ])
 
-# Create a DataLoader for inference
-inference_dataset = datasets.ImageFolder(root='dataset', transform=transform)
-inference_loader = DataLoader(inference_dataset, batch_size=1, shuffle=False)
+dataset = datasets.ImageFolder(root='dataset', transform=transform)
+loader = DataLoader(dataset, batch_size=1, shuffle=False)
 
-# Run inference
-model.eval()  # Set the model to evaluation mode
+model.eval()
 results = []
 
 predictions = []
 
 with torch.no_grad():
-    for batch_idx, (data, entry) in enumerate(zip(inference_loader, inference_dataset.samples)):
+    for batch_idx, (data, entry) in enumerate(zip(loader, dataset.samples)):
         data_resized = data[0].to(DEVICE)
         output = model(data_resized).item()
 
@@ -68,8 +58,7 @@ with torch.no_grad():
         predictions.append(prediction)
 
 
-# Save the predictions as a JSON file
-output_file = "inference_results.json"
+output_file = "predictions.json"
 with open(output_file, "w") as f:
     json.dump(predictions, f, indent=4)
 
--- trainer.py
+++ trainer.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from torch.nn import functional as F
 from torchvision import transforms
 from torchvision.datasets.folder import default_loader
-
+import torchvision.models as models
 
 def setup_device():
     DEVICE = torch.device("cpu")
@@ -21,23 +21,17 @@ def setup_device():
     return DEVICE
 
 
-class BlurDetectionModel(nn.Module):
+class BlurDetectionResNet(nn.Module):
     def __init__(self):
-        super(BlurDetectionModel, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(128 * 64 * 64, 256)
-        self.fc2 = nn.Linear(256, 1)
+        super(BlurDetectionResNet, self).__init__()
+        self.resnet = models.resnet18()
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, 1)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = torch.sigmoid(self.fc2(x))
-        return x
+        x = self.resnet(x)
+        return torch.sigmoid(x)
+
 
 
 class BlurrySharpDataset(torch.utils.data.Dataset):
@@ -50,20 +44,18 @@ class BlurrySharpDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         img_path = self.data.iloc[idx, 0]
-        label = self.data.iloc[idx, 1]
+        label_str = self.data.iloc[idx, 1]
 
         image = default_loader(img_path)
         if self.transform:
             image = self.transform(image)
 
+        label = 1 if label_str == 'blurry' else 0
         return image, label
 
 
-def preprocess_target_values(target):
-    return torch.tensor([1 if label == 'blurry' else 0 for label in target])
-
-
-def train_blur_detection_model(annotations_path, output_model_path, num_epochs=70):
+def train_blur_detection_model(annotations_path, output_model_path):
+    EPOCH = 10
     DEVICE = setup_device()
 
     annotations = pd.read_csv(annotations_path)
@@ -76,31 +68,31 @@ def train_blur_detection_model(annotations_path, output_model_path, num_epochs=7
     train_dataset = BlurrySharpDataset(annotations, transform=transform)
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
-    model = BlurDetectionModel()
+    model = BlurDetectionResNet()
     model.to(DEVICE)
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.5)
 
-    for epoch in range(num_epochs):
+    for epoch in range(EPOCH):
         for batch_idx, (data, target) in enumerate(train_loader):
             optimizer.zero_grad()
 
             data = data.to(DEVICE)
-            label_batch = preprocess_target_values(target).to(DEVICE)
+            label_batch = target.to(DEVICE).view(-1, 1)  # Reshape target labels to match model output shape
 
             output = model(data)
-            loss = criterion(output, label_batch.float().view_as(output))
+            loss = criterion(output, label_batch.float())
             loss.backward()
 
             optimizer.step()
 
-            pred = output.round()
-            correct = pred.eq(label_batch.view_as(pred)).sum().item()
+            pred = (output >= 0.5).float()  # Round predictions for accuracy calculation
+            correct = pred.eq(label_batch).sum().item()
             precision = correct / len(label_batch)
 
             if batch_idx % 100 == 0:
-                print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}, Precision: {precision:.4f}')
+                print(f'Epoch [{epoch+1}/{EPOCH}], Loss: {loss.item():.4f}, Precision: {precision:.4f}')
 
         scheduler.step()
 
